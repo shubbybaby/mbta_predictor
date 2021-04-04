84268612b6c3fc4e6f463f1bd19364409f2c6e78
Subomi Ayoola <ShubbyHome@Subomis-MacBook-Air.local>
2021-04-04 01:45:06 -0500

    fixed url link

--- mbta_time_predictor.py
+++ mbta_time_predictor.py
@@ -26,14 +26,30 @@ if not stop1 or not stop2:
 def time_predictor(entry1, entry2):
     print("Stop 1: " + entry1)
     print("stop 2: " + entry2)
-    #get api response
-    #response = requests.get("https://www.mbta.com/developers/v3-api/stops")
-    url = "https://api-v3-mbta.com/routes?filter[stop]=%s" %entry2
-    response = requests.get(url)
+    #get api response for routes stopping at stop2
+    url = "https://api-v3.mbta.com/routes?filter[stop]=place-north"
+    response =''
 
-    if response.status_code != 200:
-        print("ERROR: Server Error")
-        exit()
+    print(url)
+    try:
+        response = requests.get(url)
+        if response.status_code != 200:
+            print("ERROR: Server Error")
+            exit()
+        
+        for x in response.data:
+            desArray = x.attributes.direction_destinations
+            if desArray[0] ne entry1:
+                next
+            else
+
+    except :
+        print(" Error connecting to API, Please try again")
+    
+
+    #
+
+ 
 
 
 
