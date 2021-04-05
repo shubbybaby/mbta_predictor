b848e003c323b2bfc379ca1629bea62f1a47dcca
Subomi Ayoola <ShubbyHome@Subomis-MacBook-Air.local>
2021-04-04 21:43:28 -0500

    final update

--- mbta_time_predictor.py
+++ mbta_time_predictor.py
@@ -27,30 +27,31 @@ def time_predictor(entry1, entry2):
     print("Stop 1: " + entry1)
     print("stop 2: " + entry2)
     #get api response for routes stopping at stop2
-    url = "https://api-v3.mbta.com/routes?filter[stop]=place-north"
+    url = "https://api-v3.mbta.com/routes?filter[stop]=%s" %(entry2)
     response =''
+    print("URL: " +url)
 
-    print(url)
     try:
         response = requests.get(url)
         if response.status_code != 200:
             print("ERROR: Server Error")
             exit()
         
+        #if both stops are found, use the predictor api to get arrival time
         for x in response.data:
             desArray = x.attributes.direction_destinations
-            if desArray[0] ne entry1:
+            if desArray[0] == entry1 and desArray[1] == entry2:
+                #get arrival time from api
+                try:
+                    url_p = "https://api-v3.mbta.com/predictions?filter%5Bstop%5D=%s&filter%5Bdirection_id%5D=0&include=stop" %(entry1)
+                    response_p = requests.get(url_p)
+                    arrivalTime = response_p.data[0].attributes.arrival_times
+                except: 
+                    print(" Error getting predicted arrival time from API, Please try again")
                 next
-            else
-
-    except :
-        print(" Error connecting to API, Please try again")
+        
+    except:
+        print("Error connecting to API, Please try again")
     
-
-    #
-
- 
-
-
-
+    return 0
 time_predictor(stop1, stop2)
--- test_mbta_time_predictor.py
+++ test_mbta_time_predictor.py
@@ -1,8 +1,6 @@
 import unittest
 import mbta_time_predictor
 
-
-
 class TestPredictor(unittest.TestCase):
     def test_no_stops_entered(self):
         """
@@ -12,24 +10,19 @@ class TestPredictor(unittest.TestCase):
         self.assertEqual(mbta_time_predictor.time_predictor('',''), "An Error has occured, Please check the stop names.")
 
     def test_stop1_invalid(self):
-        pass
+        self.assertEqual(mbta_time_predictor.time_predictor('',17094), "An Error has occured, Please check the stop names.")
 
     def test_stop2_invalid(self):
-        pass
-    
+        self.assertEqual(mbta_time_predictor.time_predictor(17094,''), "An Error has occured, Please check the stop names.")
+
     def test_stop1_stop2_invalid(self):
-        pass
+        self.assertEqual(mbta_time_predictor.time_predictor('a','b'), "An Error has occured, Please check the stop names.")
 
     def test_stop1_stop2_not_on_same_route(self):
         pass
     
     def test_time_predicted_success(self):
-        self.assertEqual(mbta_time_predictor.time_predictor('Cleveland Circle','North station'), "An Error has occured, Please check the stop names.")
-
-        pass
-    
-
-
+        self.assertEqual(mbta_time_predictor.time_predictor('Cleveland Circle','North station'), 0)
 
 
 if __name__ == '__main__':
